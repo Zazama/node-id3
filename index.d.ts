1f095a7178da4b07dfc5868c9e33fc84a602e261
Zazama <jan.metzger@mailbox.org>
2020-10-22 18:18:33 +0200

    update typescript definition & bump version

--- index.d.ts
+++ index.d.ts
@@ -2,46 +2,46 @@ declare module "node-id3" {
    namespace NodeID3 {
       export interface Tags {
          /**
-          * The 'Album/Movie/Show title' frame is intended for the title of the recording(/source of sound) which the audio in the file is taken from. 
+          * The 'Album/Movie/Show title' frame is intended for the title of the recording(/source of sound) which the audio in the file is taken from.
           */
          album?: string,
          /**
-          * The 'BPM' frame contains the number of beats per minute in the mainpart of the audio. The BPM is an integer and represented as a numerical string. 
+          * The 'BPM' frame contains the number of beats per minute in the mainpart of the audio. The BPM is an integer and represented as a numerical string.
           */
          bpm?: string,
          /**
-          *  The 'Composer(s)' frame is intended for the name of the composer(s). They are seperated with the "/" character. 
+          *  The 'Composer(s)' frame is intended for the name of the composer(s). They are seperated with the "/" character.
           */
          composer?: string,
          /**
           * The 'Content type', which previously was stored as a one byte numeric value only, is now a numeric string. You may use one or several of the types as ID3v1.1 did or, since the category list would be impossible to maintain with accurate and up to date categories, define your own.
-          * 
+          *
           * References to the ID3v1 genres can be made by, as first byte, enter "(" followed by a number from the genres list (appendix A) and ended with a ")" character. This is optionally followed by a refinement, e.g. "(21)" or "(4)Eurodisco". Several references can be made in the same frame, e.g. "(51)(39)". If the refinement should begin with a "(" character it should be replaced with "((", e.g. "((I can figure out any genre)" or "(55)((I think...)"
           */
          genre?: string,
          /**
-          * The 'Copyright message' frame, which must begin with a year and a space character (making five characters), is intended for the copyright holder of the original sound, not the audio file itself. The absence of this frame means only that the copyright information is unavailable or has been removed, and must not be interpreted to mean that the sound is public domain. Every time this field is displayed the field must be preceded with "Copyright © ". 
+          * The 'Copyright message' frame, which must begin with a year and a space character (making five characters), is intended for the copyright holder of the original sound, not the audio file itself. The absence of this frame means only that the copyright information is unavailable or has been removed, and must not be interpreted to mean that the sound is public domain. Every time this field is displayed the field must be preceded with "Copyright © ".
           */
          copyright?: string,
          /**
-          * The 'Date' frame is a numeric string in the DDMM format containing the date for the recording. This field is always four characters long. 
+          * The 'Date' frame is a numeric string in the DDMM format containing the date for the recording. This field is always four characters long.
           */
          date?: string,
          /**
-          * The 'Playlist delay' defines the numbers of milliseconds of silence between every song in a playlist. The player should use the "ETC" frame, if present, to skip initial silence and silence at the end of the audio to match the 'Playlist delay' time. The time is represented as a numeric string. 
+          * The 'Playlist delay' defines the numbers of milliseconds of silence between every song in a playlist. The player should use the "ETC" frame, if present, to skip initial silence and silence at the end of the audio to match the 'Playlist delay' time. The time is represented as a numeric string.
           */
          playlistDelay?: string,
          /**
-          * The 'Encoded by' frame contains the name of the person or organisation that encoded the audio file. This field may contain a copyright message, if the audio file also is copyrighted by the encoder. 
+          * The 'Encoded by' frame contains the name of the person or organisation that encoded the audio file. This field may contain a copyright message, if the audio file also is copyrighted by the encoder.
           */
          encodedBy?: string,
          /**
-          * The 'Lyricist(s)/Text writer(s)' frame is intended for the writer(s) of the text or lyrics in the recording. They are seperated with the "/" character. 
+          * The 'Lyricist(s)/Text writer(s)' frame is intended for the writer(s) of the text or lyrics in the recording. They are seperated with the "/" character.
           */
          textWriter?: string,
          /**
           * The 'File type' frame indicates which type of audio this tag defines. The following type and refinements are defined:
-          * 
+          *
           * MPG       MPEG Audio
           * /1        MPEG 1/2 layer I
           * /2        MPEG 1/2 layer II
@@ -50,36 +50,36 @@ declare module "node-id3" {
           *  /AAC     Advanced audio compression
           * VQF       Transform-domain Weighted Interleave Vector Quantization
           * PCM       Pulse Code Modulated audio
-          * 
-          * but other types may be used, not for these types though. This is used in a similar way to the predefined types in the "Media type" frame, but without parentheses. If this frame is not present audio type is assumed to be "MPG". 
+          *
+          * but other types may be used, not for these types though. This is used in a similar way to the predefined types in the "Media type" frame, but without parentheses. If this frame is not present audio type is assumed to be "MPG".
           */
          fileType?: string,
          /**
-          * The 'Time' frame is a numeric string in the HHMM format containing the time for the recording. This field is always four characters long. 
+          * The 'Time' frame is a numeric string in the HHMM format containing the time for the recording. This field is always four characters long.
           */
          time?: string,
          /**
-          * The 'Content group description' frame is used if the sound belongs to a larger category of sounds/music. For example, classical music is often sorted in different musical sections (e.g. "Piano Concerto", "Weather - Hurricane"). 
+          * The 'Content group description' frame is used if the sound belongs to a larger category of sounds/music. For example, classical music is often sorted in different musical sections (e.g. "Piano Concerto", "Weather - Hurricane").
           */
          contentGroup?: string,
          /**
-          * The 'Title/Songname/Content description' frame is the actual name of the piece (e.g. "Adagio", "Hurricane Donna"). 
+          * The 'Title/Songname/Content description' frame is the actual name of the piece (e.g. "Adagio", "Hurricane Donna").
           */
          title?: string,
          /**
-          * The 'Subtitle/Description refinement' frame is used for information directly related to the contents title (e.g. "Op. 16" or "Performed live at Wembley"). 
+          * The 'Subtitle/Description refinement' frame is used for information directly related to the contents title (e.g. "Op. 16" or "Performed live at Wembley").
           */
          subtitle?: string,
          /**
-          * The 'Initial key' frame contains the musical key in which the sound starts. It is represented as a string with a maximum length of three characters. The ground keys are represented with "A","B","C","D","E", "F" and "G" and halfkeys represented with "b" and "#". Minor is represented as "m". Example "Cbm". Off key is represented with an "o" only. 
+          * The 'Initial key' frame contains the musical key in which the sound starts. It is represented as a string with a maximum length of three characters. The ground keys are represented with "A","B","C","D","E", "F" and "G" and halfkeys represented with "b" and "#". Minor is represented as "m". Example "Cbm". Off key is represented with an "o" only.
           */
          initialKey?: string,
          /**
-          * The 'Language(s)' frame should contain the languages of the text or lyrics spoken or sung in the audio. The language is represented with three characters according to ISO-639-2. If more than one language is used in the text their language codes should follow according to their usage. 
+          * The 'Language(s)' frame should contain the languages of the text or lyrics spoken or sung in the audio. The language is represented with three characters according to ISO-639-2. If more than one language is used in the text their language codes should follow according to their usage.
           */
          language?: string,
          /**
-          * The 'Length' frame contains the length of the audiofile in milliseconds, represented as a numeric string. 
+          * The 'Length' frame contains the length of the audiofile in milliseconds, represented as a numeric string.
           */
          length?: string,
          /**
@@ -169,83 +169,83 @@ declare module "node-id3" {
           */
          mediaType?: string,
          /**
-          * The 'Original album/movie/show title' frame is intended for the title of the original recording (or source of sound), if for example the music in the file should be a cover of a previously released song. 
+          * The 'Original album/movie/show title' frame is intended for the title of the original recording (or source of sound), if for example the music in the file should be a cover of a previously released song.
           */
          originalTitle?: string,
          /**
-          * The 'Original filename' frame contains the preferred filename for the file, since some media doesn't allow the desired length of the filename. The filename is case sensitive and includes its suffix. 
+          * The 'Original filename' frame contains the preferred filename for the file, since some media doesn't allow the desired length of the filename. The filename is case sensitive and includes its suffix.
           */
          originalFilename?: string,
          /**
-          * The 'Original lyricist(s)/text writer(s)' frame is intended for the text writer(s) of the original recording, if for example the music in the file should be a cover of a previously released song. The text writers are seperated with the "/" character. 
+          * The 'Original lyricist(s)/text writer(s)' frame is intended for the text writer(s) of the original recording, if for example the music in the file should be a cover of a previously released song. The text writers are seperated with the "/" character.
           */
          originalTextwriter?: string,
          /**
-          * The 'Original artist(s)/performer(s)' frame is intended for the performer(s) of the original recording, if for example the music in the file should be a cover of a previously released song. The performers are seperated with the "/" character. 
+          * The 'Original artist(s)/performer(s)' frame is intended for the performer(s) of the original recording, if for example the music in the file should be a cover of a previously released song. The performers are seperated with the "/" character.
           */
          originalArtist?: string,
          /**
-          * The 'Original release year' frame is intended for the year when the original recording, if for example the music in the file should be a cover of a previously released song, was released. The field is formatted as in the "Year" frame. 
+          * The 'Original release year' frame is intended for the year when the original recording, if for example the music in the file should be a cover of a previously released song, was released. The field is formatted as in the "Year" frame.
           */
          originalYear?: string,
          /**
-          * The 'File owner/licensee' frame contains the name of the owner or licensee of the file and it's contents. 
+          * The 'File owner/licensee' frame contains the name of the owner or licensee of the file and it's contents.
           */
          fileOwner?: string,
          /**
-          * The 'Lead artist(s)/Lead performer(s)/Soloist(s)/Performing group' is used for the main artist(s). They are seperated with the "/" character. 
+          * The 'Lead artist(s)/Lead performer(s)/Soloist(s)/Performing group' is used for the main artist(s). They are seperated with the "/" character.
           */
          artist?: string,
          /**
-          * The 'Band/Orchestra/Accompaniment' frame is used for additional information about the performers in the recording. 
+          * The 'Band/Orchestra/Accompaniment' frame is used for additional information about the performers in the recording.
           */
          performerInfo?: string,
          /**
-          * The 'Conductor' frame is used for the name of the conductor. 
+          * The 'Conductor' frame is used for the name of the conductor.
           */
          conductor?: string,
          /**
-          * The 'Interpreted, remixed, or otherwise modified by' frame contains more information about the people behind a remix and similar interpretations of another existing piece. 
+          * The 'Interpreted, remixed, or otherwise modified by' frame contains more information about the people behind a remix and similar interpretations of another existing piece.
           */
          remixArtist?: string,
          /**
-          * The 'Part of a set' frame is a numeric string that describes which part of a set the audio came from. This frame is used if the source described in the "Album/Movie/Show title" frame is divided into several mediums, e.g. a double CD. The value may be extended with a "/" character and a numeric string containing the total number of parts in the set. E.g. "1/2". 
+          * The 'Part of a set' frame is a numeric string that describes which part of a set the audio came from. This frame is used if the source described in the "Album/Movie/Show title" frame is divided into several mediums, e.g. a double CD. The value may be extended with a "/" character and a numeric string containing the total number of parts in the set. E.g. "1/2".
           */
          partOfSet?: string,
          /**
-          * The 'Publisher' frame simply contains the name of the label or publisher. 
+          * The 'Publisher' frame simply contains the name of the label or publisher.
           */
          publisher?: string,
          /**
-          * The 'Track number/Position in set' frame is a numeric string containing the order number of the audio-file on its original recording. This may be extended with a "/" character and a numeric string containing the total numer of tracks/elements on the original recording. E.g. "4/9". 
+          * The 'Track number/Position in set' frame is a numeric string containing the order number of the audio-file on its original recording. This may be extended with a "/" character and a numeric string containing the total numer of tracks/elements on the original recording. E.g. "4/9".
           */
          trackNumber?: string,
          /**
-          * The 'Recording dates' frame is a intended to be used as complement to the "Year", "Date" and "Time" frames. E.g. "4th-7th June, 12th June" in combination with the "Year" frame. 
+          * The 'Recording dates' frame is a intended to be used as complement to the "Year", "Date" and "Time" frames. E.g. "4th-7th June, 12th June" in combination with the "Year" frame.
           */
          recordingDates?: string,
          /**
-          * The 'Internet radio station name' frame contains the name of the internet radio station from which the audio is streamed. 
+          * The 'Internet radio station name' frame contains the name of the internet radio station from which the audio is streamed.
           */
          internetRadioName?: string,
          /**
-          * The 'Internet radio station owner' frame contains the name of the owner of the internet radio station from which the audio is streamed. 
+          * The 'Internet radio station owner' frame contains the name of the owner of the internet radio station from which the audio is streamed.
           */
          internetRadioOwner?: string,
          /**
-          * The 'Size' frame contains the size of the audiofile in bytes, excluding the ID3v2 tag, represented as a numeric string. 
+          * The 'Size' frame contains the size of the audiofile in bytes, excluding the ID3v2 tag, represented as a numeric string.
           */
          size?: string,
          /**
-          * The 'ISRC' frame should contain the International Standard Recording Code (ISRC) (12 characters). 
+          * The 'ISRC' frame should contain the International Standard Recording Code (ISRC) (12 characters).
           */
          ISRC?: string,
          /**
-          * The 'Software/Hardware and settings used for encoding' frame includes the used audio encoder and its settings when the file was encoded. Hardware refers to hardware encoders, not the computer on which a program was run. 
+          * The 'Software/Hardware and settings used for encoding' frame includes the used audio encoder and its settings when the file was encoded. Hardware refers to hardware encoders, not the computer on which a program was run.
           */
          encodingTechnology?: string,
          /**
-          * The 'Year' frame is a numeric string with a year of the recording. This frames is always four characters long (until the year 10000). 
+          * The 'Year' frame is a numeric string with a year of the recording. This frames is always four characters long (until the year 10000).
           */
          year?: string,
          comment?: {
@@ -288,15 +288,17 @@ declare module "node-id3" {
             elementID: string,
             endTimeMs: number,
             startTimeMs: number,
-            tags?: {
-               image?: Tags["image"],
-               title?: string,
-            },
+            tags?: Tags
          }>,
-
+         tableOfContents?: Array<{
+            elementID: string,
+            isOrdered?: boolean,
+            elements?: Array<string>
+            tags?: Tags
+         }>,
          /**
           * The 'Commercial information' frame is a URL pointing at a webpage with information such as where the album can be bought. There may be more than one "WCOM" frame in a tag, but not with the same content.
-          */         
+          */
          commercialUrl?: Array<string>,
          /**
           * The 'Copyright/Legal information' frame is a URL pointing at a webpage where the terms of use and ownership of the file is described.
