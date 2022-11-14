06f4b4e49a1c68f7eba8aebead47d6b464413af2
Patrick Bricout <pbricout@users.noreply.github.com>
2022-11-14 17:23:25 +0100

    Add ETCO and COMR frame constants
    Names are maybe not the best, but they are close to the documentation.

--- src/ID3Definitions.js
+++ src/ID3Definitions.js
@@ -227,6 +227,62 @@ const Constants = {
         MILLISECONDS: 2
     },
     /**
+     * `ETCO` tag frame
+     */
+    EventTimingCodes: {
+        EventType: {
+            /**
+             * Padding has no meaning
+             */
+            PADDING: 0x00,
+            END_OF_INITIAL_SILENCE: 0x01,
+            INTRO_START: 0x02,
+            MAINPART_START: 0x03,
+            OUTRO_START: 0x04,
+            OUTRO_END: 0x05,
+            VERSE_START: 0x06,
+            REFRAIN_START: 0x07,
+            INTERLUDE_START: 0x08,
+            THEME_START: 0x09,
+            VARIATION_START: 0x0A,
+            KEY_CHANGE: 0x0B,
+            TIME_CHANGE: 0x0C,
+            /**
+             * (Snap, Crackle & Pop)
+             */
+            MOMENTARY_UNWANTED_NOISE: 0x0D,
+            SUSTAINED_NOISE: 0x0E,
+            SUSTAINED_NOISE_END: 0x0F,
+            INTRO_END: 0x10,
+            MAINPART_END: 0x11,
+            VERSE_END: 0x12,
+            REFRAIN_END: 0x013,
+            THEME_END: 0x14,
+            /**
+             * $15-$DF reserved for future use
+             */
+            RESERVED_1: 0x15,
+            /**
+             * $E0-$EF not predefined sync 0-F
+             */
+            NOT_PREDEFINED_SYNC: 0xE0,
+            /**
+             * $F0-$FC reserved for future use
+             */
+            RESERVED_2: 0xF0,
+            /**
+             * Start of silence
+             */
+            AUDIO_END: 0xFD,
+            AUDIO_FILE_ENDS: 0xFE,
+            /**
+             * one more byte of events follows (all the following bytes with
+             * the value $FF have the same function)
+             */
+            ONE_MORE_BYTE_FOLLOWS: 0xFF
+        }
+    },
+    /**
      * `SYLT` tag frame
      */
     SynchronisedLyrics: {
@@ -275,6 +331,22 @@ const Constants = {
             ILLUSTRATION: 0x12,
             BAND_OR_ARTIST_LOGOTYPE: 0x13,
             PUBLISHER_OR_STUDIO_LOGOTYPE: 0x14
+        }
+    },
+    /**
+     * `COMR` tag frame
+     */
+    CommercialFrame: {
+        ReceivedAs: {
+            OTHER: 0x00,
+            STANDARD_CD_ALBUM_WITH_OTHER_SONGS: 0x01,
+            COMPRESSED_AUDIO_ON_CD: 0x02,
+            FILE_OVER_THE_INTERNET: 0x03,
+            STREAM_OVER_THE_INTERNET: 0x04,
+            AS_NOTE_SHEETS: 0x05,
+            AS_NOTE_SHEETS_IN_A_BOOK_WITH_OTHER_SHEETS: 0x06,
+            MUSIC_ON_OTHER_MEDIA: 0x07,
+            NON_MUSICAL_MERCHANDISE: 0x08
         }
     }
 }
