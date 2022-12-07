b1b6332983c2f1f5407f0d7d0cf7b5b2f17ac5c2
Patrick Bricout <pbricout@users.noreply.github.com>
2022-12-07 05:28:43 +0100

    Convert ID3Helpers to Typescript
    Add a utility isKeyOf().

--- src/ID3Helpers.ts
+++ src/ID3Helpers.ts
@@ -3,24 +3,25 @@ import {
     FRAME_ALIASES
 } from "./definitions/FrameIdentifiers"
 import * as ID3Frames from './ID3Frames'
-const ID3Util = require('./ID3Util')
+import * as ID3Util from './ID3Util'
 import { Frame } from './Frame'
 import { getFrameSize } from './FrameHeader'
+import { Options } from "./types/Options"
+import { Tags, RawTags, WriteTags } from './types/Tags'
+import { isKeyOf } from "./util"
 
 /**
  * Returns array of buffers created by tags specified in the tags argument
- * @param tags - Object containing tags to be written
- * @returns {Array}
  */
-function createBuffersFromTags(tags) {
-    const frames = []
+function createBuffersFromTags(tags: WriteTags) {
+    const frames: Buffer[] = []
     if(!tags) {
         return frames
     }
-    const rawObject = Object.keys(tags).reduce((acc, val) => {
-        if(FRAME_IDENTIFIERS.v3[val] !== undefined) {
-            acc[FRAME_IDENTIFIERS.v3[val]] = tags[val]
-        } else if(FRAME_IDENTIFIERS.v4[val] !== undefined) {
+    const rawObject = Object.entries(tags).reduce<RawTags>((acc, [key, value]) => {
+        if (isKeyOf(key, FRAME_IDENTIFIERS.v3)) {
+            acc[FRAME_IDENTIFIERS.v3[key]] = value
+        } else if (isKeyOf(key, FRAME_IDENTIFIERS.v4)) {
             // Currently, node-id3 always writes ID3 version 3.
             // However, version 3 and 4 are very similar, and node-id3
             // can also read version 4 frames.
@@ -28,35 +29,38 @@ function createBuffersFromTags(tags) {
             // allow writing version 4 frames into a version 3 tag.
             // If a reader does not support a v4 frame, it's (per spec)
             // supposed to skip it, so it should not be a problem.
-            acc[FRAME_IDENTIFIERS.v4[val]] = tags[val]
+            acc[FRAME_IDENTIFIERS.v4[key]] = value
         } else {
-            acc[val] = tags[val]
+            acc[key as keyof RawTags] = value
         }
         return acc
     }, {})
 
-    Object.keys(rawObject).forEach((frameIdentifier) => {
-        let frame
+    Object.entries(rawObject).forEach(([frameIdentifier, data]) => {
         // Check if invalid frameIdentifier
         if (frameIdentifier.length !== 4) {
             return
         }
-        if (frameIdentifier in ID3Frames.Frames) {
-            frame = ID3Frames.Frames[frameIdentifier].create(rawObject[frameIdentifier], 3)
+        let frame
+        if (isKeyOf(frameIdentifier, ID3Frames.Frames)) {
+            frame = ID3Frames.Frames[frameIdentifier].create(data)
         } else if (frameIdentifier.startsWith('T')) {
-            frame = ID3Frames.GENERIC_TEXT.create(frameIdentifier, rawObject[frameIdentifier], 3)
+            frame = ID3Frames.GENERIC_TEXT.create(frameIdentifier, data)
         } else if (frameIdentifier.startsWith('W')) {
-            if(ID3Util.getSpecOptions(frameIdentifier, 3).multiple && rawObject[frameIdentifier] instanceof Array && rawObject[frameIdentifier].length > 0) {
-                frame = Buffer.alloc(0)
+            if (
+                ID3Util.getSpecOptions(frameIdentifier).multiple &&
+                data instanceof Array &&
+                data.length
+            ) {
                 // deduplicate array
-                for(const url of [...new Set(rawObject[frameIdentifier])]) {
-                    frame = Buffer.concat([frame, ID3Frames.GENERIC_URL.create(frameIdentifier, url, 3)])
-                }
+                const frames = [...new Set(data)].map(
+                    url => ID3Frames.GENERIC_URL.create(frameIdentifier, url)
+                ).filter((frame): frame is Buffer => !!frame)
+                frame = Buffer.concat(frames)
             } else {
-                frame = ID3Frames.GENERIC_URL.create(frameIdentifier, rawObject[frameIdentifier], 3)
+                frame = ID3Frames.GENERIC_URL.create(frameIdentifier, data)
             }
         }
-
         if (frame && frame instanceof Buffer) {
             frames.push(frame)
         }
@@ -67,41 +71,39 @@ function createBuffersFromTags(tags) {
 
 /**
  * Return a buffer with the frames for the specified tags
- * @param tags - Object containing tags to be written
- * @returns {Buffer}
  */
-export function createBufferFromTags(tags) {
+export function createBufferFromTags(tags: WriteTags) {
     return Buffer.concat(createBuffersFromTags(tags))
 }
 
-export function getTagsFromBuffer(filebuffer, options) {
-    const framePosition = ID3Util.getFramePosition(filebuffer)
+export function getTagsFromBuffer(buffer: Buffer, options: Options) {
+    const framePosition = ID3Util.getFramePosition(buffer)
     if(framePosition === -1) {
         return getTagsFromFrames([], 3, options)
     }
-    const frameSize = ID3Util.decodeSize(filebuffer.slice(framePosition + 6, framePosition + 10)) + 10
+    const frameSize = ID3Util.decodeSize(buffer.subarray(framePosition + 6, framePosition + 10)) + 10
     const frame = Buffer.alloc(frameSize + 1)
-    filebuffer.copy(frame, 0, framePosition)
+    buffer.copy(frame, 0, framePosition)
     //ID3 version e.g. 3 if ID3v2.3.0
     const version = frame[3]
     const tagFlags = ID3Util.parseTagHeaderFlags(frame)
     let extendedHeaderOffset = 0
     if(tagFlags.extendedHeader) {
         if(version === 3) {
-            extendedHeaderOffset = 4 + filebuffer.readUInt32BE(10)
+            extendedHeaderOffset = 4 + buffer.readUInt32BE(10)
         } else if(version === 4) {
-            extendedHeaderOffset = ID3Util.decodeSize(filebuffer.slice(10, 14))
+            extendedHeaderOffset = ID3Util.decodeSize(buffer.subarray(10, 14))
         }
     }
     const frameBody = Buffer.alloc(frameSize - 10 - extendedHeaderOffset)
-    filebuffer.copy(frameBody, 0, framePosition + 10 + extendedHeaderOffset)
+    buffer.copy(frameBody, 0, framePosition + 10 + extendedHeaderOffset)
 
     const frames = getFramesFromTagBody(frameBody, version, options)
 
     return getTagsFromFrames(frames, version, options)
 }
 
-function isFrameDiscarded(frameIdentifier, options) {
+function isFrameDiscarded(frameIdentifier: string, options: Options) {
     if(options.exclude instanceof Array && options.exclude.includes(frameIdentifier)) {
         return true
     }
@@ -109,7 +111,7 @@ function isFrameDiscarded(frameIdentifier, options) {
     return options.include instanceof Array && !options.include.includes(frameIdentifier)
 }
 
-function getFramesFromTagBody(tagBody, version, options = {}) {
+function getFramesFromTagBody(tagBody: Buffer, version: number, options = {}) {
     if(!(tagBody instanceof Buffer)) {
         return []
     }
@@ -134,41 +136,39 @@ function getFramesFromTagBody(tagBody, version, options = {}) {
     return frames
 }
 
-function getTagsFromFrames(frames, version, options = {}) {
-    const tags = { }
-    const raw = { }
+function getTagsFromFrames(
+    frames: Frame[],
+    _version: number,
+    options: Options = {}
+) {
+    const tags: Tags = {}
+    const raw: RawTags = {}
 
-    frames.forEach((frame) => {
-        const frameValue = frame.getValue()
-        const frameAlias = FRAME_ALIASES.v34[frame.identifier]
+    const push = (dest: unknown, value: unknown) => {
+        const destArray = Array.isArray(dest) ? dest : []
+        destArray.push(value)
+        return destArray as never
+    }
+    const set = (_dest: unknown, value: unknown) => value as never
 
-        if(ID3Util.getSpecOptions(frame.identifier, version).multiple) {
-            if(!options.onlyRaw) {
-                if(!tags[frameAlias]) {
-                    tags[frameAlias] = []
-                }
-                tags[frameAlias].push(frameValue)
-            }
-            if(!options.noRaw) {
-                if(!raw[frame.identifier]) {
-                    raw[frame.identifier] = []
-                }
-                raw[frame.identifier].push(frameValue)
-            }
-        } else {
-            if(!options.onlyRaw) {
-                tags[frameAlias] = frameValue
-            }
-            if(!options.noRaw) {
-                raw[frame.identifier] = frameValue
-            }
+    frames.forEach(frame => {
+        const identifier = frame.identifier as keyof RawTags
+        const frameAlias = FRAME_ALIASES.v34[identifier] as keyof Tags
+
+        const assign = ID3Util.getSpecOptions(identifier).multiple ? push : set
+
+        if (!options.onlyRaw) {
+            tags[frameAlias] = assign(tags[frameAlias], frame.getValue())
+        }
+        if (!options.noRaw) {
+            raw[identifier] = assign(raw[identifier], frame.getValue())
         }
     })
 
-    if(options.onlyRaw) {
+    if (options.onlyRaw) {
         return raw
     }
-    if(options.noRaw) {
+    if (options.noRaw) {
         return tags
     }
 
@@ -176,6 +176,6 @@ function getTagsFromFrames(frames, version, options = {}) {
     return tags
 }
 
-export function getTagsFromID3Body(body) {
+export function getTagsFromID3Body(body: Buffer) {
     return getTagsFromFrames(getFramesFromTagBody(body, 3), 3)
 }
--- src/util.ts
+++ src/util.ts
@@ -5,3 +5,9 @@ export const isFunction =
 
 export const isString = (value: unknown): value is string =>
     typeof value === 'string' || value instanceof String
+
+export const isKeyOf = <T extends Record<string, unknown>>(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    key: keyof any,
+    object: T
+): key is keyof T => key in object
