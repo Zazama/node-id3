4a475031f6833da3affa898062484c6528e441f8
Patrick Bricout <pbricout@users.noreply.github.com>
2022-12-09 09:59:54 +0100

    Simplify index exports
    Anything in these files should be exported, let's simplify.
    API Extractor examples look like this.

--- index.ts
+++ index.ts
@@ -1,49 +1,13 @@
 // Used specification: http://id3.org/id3v2.3.0
 
-// Types
+export * from "./src/types/Tags"
+export * from "./src/types/Options"
 
-export {
-    Tags,
-    TagAliases,
-    TagIdentifiers,
-    WriteTags
-} from "./src/types/Tags"
-export {
-    Options
-} from "./src/types/Options"
+export * from './src/definitions/TagConstants'
 
-// Definitions
-
-export {
-    TagConstants
-} from './src/definitions/TagConstants'
-
-// Operations
-
-export {
-    create,
-    CreateCallback
-} from "./src/api/create"
-export {
-    read,
-    ReadCallback
-} from "./src/api/read"
-export {
-    removeTags,
-    removeTagsFromBuffer,
-    RemoveCallback
-} from "./src/api/remove"
-export {
-    update
-} from "./src/api/update"
-export {
-    write,
-    WriteCallback
-} from "./src/api/write"
-
-// Promises
-
-export {
-    Promises,
-    Promise
-} from "./src/api/promises"
+export * from "./src/api/create"
+export * from "./src/api/read"
+export * from "./src/api/remove"
+export * from "./src/api/update"
+export * from "./src/api/write"
+export * from "./src/api/promises"
