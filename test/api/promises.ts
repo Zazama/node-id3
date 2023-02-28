fb711da856a54075d9e23d7e8b40ecc858cb5cb4
Patrick Bricout <pbricout@users.noreply.github.com>
2023-02-28 21:30:38 +0100

    Use Promises instead of deprecated Promise in tests

--- test/api/promises.ts
+++ test/api/promises.ts
@@ -5,19 +5,19 @@ import chaiAsPromised = require('chai-as-promised')
 const { expect } = chai
 chai.use(chaiAsPromised)
 
-describe('NodeID3 API Promise', function () {
+describe('NodeID3 API Promises', function () {
     const invalidFilepath = 'should-hopefully-not-be-a-valid-file.mp3'
     const testFilepath = 'write-promise-test-file.mp3'
     describe('#create()', function () {
         it('resolve', function () {
             return expect(
-                NodeID3.Promise.create({})
+                NodeID3.Promises.create({})
             ).to.eventually.be.instanceOf(Buffer)
         })
     })
     const writeTestCases = [
-        ['#write()', NodeID3.Promise.write],
-        ['#update()', NodeID3.Promise.update]
+        ['#write()', NodeID3.Promises.write],
+        ['#update()', NodeID3.Promises.update]
     ] as const
     writeTestCases.forEach(([name, fn]) => {
         describe(name, function () {
@@ -54,14 +54,14 @@ describe('NodeID3 API Promise', function () {
         describe('with buffer', function() {
             it('resolve', function () {
                 return expect(
-                    NodeID3.Promise.read(Buffer.alloc(0))
+                    NodeID3.Promises.read(Buffer.alloc(0))
                 ).to.eventually.to.have.key('raw')
             })
         })
         describe('with invalid file path', function() {
             it('reject', function () {
                 return expect(
-                    NodeID3.Promise.read(invalidFilepath)
+                    NodeID3.Promises.read(invalidFilepath)
                 ).to.eventually.be.rejectedWith(Error)
             })
         })
@@ -71,7 +71,7 @@ describe('NodeID3 API Promise', function () {
             })
             it('resolve', function () {
                 return expect(
-                    NodeID3.Promise.read(testFilepath)
+                    NodeID3.Promises.read(testFilepath)
                 ).to.eventually.to.have.key('raw')
             })
             after(function() {
@@ -83,7 +83,7 @@ describe('NodeID3 API Promise', function () {
         describe('with invalid file path', function() {
             it('reject', function () {
                 return expect(
-                    NodeID3.Promise.removeTags(invalidFilepath)
+                    NodeID3.Promises.removeTags(invalidFilepath)
                 ).to.eventually.be.rejectedWith(Error)
             })
         })
@@ -93,7 +93,7 @@ describe('NodeID3 API Promise', function () {
             })
             it('resolve', function () {
                 return expect(
-                    NodeID3.Promise.removeTags(testFilepath)
+                    NodeID3.Promises.removeTags(testFilepath)
                 ).to.eventually.be.fulfilled
             })
             after(function() {
