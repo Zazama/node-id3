3afbfd7833672bd808987ad8c1ffea77da3dc5e4
Patrick Bricout <pbricout@users.noreply.github.com>
2023-03-21 23:25:24 +0100

    Sort frame options
    - split by version
    - sort alphabetically so it is easier to search and compare
      with filenames frame-xxx

--- src/definitions/FrameOptions.ts
+++ src/definitions/FrameOptions.ts
@@ -3,36 +3,37 @@ export type FrameOptions = {
     updateCompareKey?: string
 }
 
-export const FRAME_OPTIONS = {
+const FRAME_OPTIONS_V2 = {
     "PIC": {
         multiple: false /* change in 1.0 */
-    },
-    "WCM": {
-        multiple: true
     },
     "WAR": {
         multiple: true
     },
-    "T___": {
-        // This is "correct", but in v4, the text frame's value can be split by using 0x00.
-        // https://github.com/Zazama/node-id3/issues/111
-        multiple: false
-    },
-    "TXXX": {
-        multiple: true,
-        updateCompareKey: "description"
-    },
+    "WCM": {
+        multiple: true
+    }
+ } as const satisfies Record<string, FrameOptions>
+
+
+const FRAME_OPTIONS_V3 = {
     "APIC": {
         multiple: false /* change in 1.0 */
     },
-    "USLT": {
-        multiple: false /* change in 1.0 */
-    },
-    "SYLT": {
+    "CHAP": {
         multiple: true
     },
     "COMM": {
         multiple: false /* change in 1.0 */
+    },
+    "COMR": {
+        multiple: true
+    },
+    "CTOC": {
+        multiple: true
+    },
+    "ETCO": {
+        multiple: false
     },
     "POPM": {
         multiple: false /* change in 1.0 */
@@ -40,15 +41,23 @@ export const FRAME_OPTIONS = {
     "PRIV": {
         multiple: true
     },
-    "CTOC": {
+    "SYLT": {
         multiple: true
     },
-    "CHAP": {
-        multiple: true
-    },
-    "WXXX": {
+    "TXXX": {
         multiple: true,
         updateCompareKey: "description"
+    },
+    "T___": {
+        // This is "correct", but in v4, the text frame's value can be split by using 0x00.
+        // https://github.com/Zazama/node-id3/issues/111
+        multiple: false
+    },
+    "UFID": {
+        multiple: true
+    },
+    "USLT": {
+        multiple: false /* change in 1.0 */
     },
     "WCOM": {
         multiple: true
@@ -56,13 +65,13 @@ export const FRAME_OPTIONS = {
     "WOAR": {
         multiple: true
     },
-    "ETCO": {
-        multiple: false
-    },
-    "COMR": {
-        multiple: true
-    },
-    "UFID": {
-        multiple: true
+    "WXXX": {
+        multiple: true,
+        updateCompareKey: "description"
     }
  } as const satisfies Record<string, FrameOptions>
+
+ export const FRAME_OPTIONS = {
+    ...FRAME_OPTIONS_V2,
+    ...FRAME_OPTIONS_V3
+ } as const
