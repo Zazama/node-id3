340094073f17bea0aeccbb2f85af500bd92da5ef
Patrick Bricout <pbricout@users.noreply.github.com>
2023-03-26 17:25:42 +0200

    Type and cleanup COMM comment frame

--- src/frames/frame-comm.ts
+++ src/frames/frame-comm.ts
@@ -1,12 +1,12 @@
+import { TextEncoding } from "../definitions/Encoding"
 import { FrameBuilder } from "../FrameBuilder"
 import { FrameReader } from "../FrameReader"
 import { Comment } from "../types/TagFrames"
 
 export const COMM = {
     create: (data: Comment) => {
-        data = data || {}
         if(!data.text) {
-            return null
+            throw new TypeError("Missing text from 'Comment' frame")
         }
 
         return new FrameBuilder("COMM")
@@ -16,11 +16,10 @@ export const COMM = {
             .appendValue(data.text, null, 0x01)
             .getBuffer()
     },
-    read: (buffer: Buffer) => {
+    read: (buffer: Buffer): Comment => {
         const reader = new FrameReader(buffer, 0)
-
         return {
-            language: reader.consumeStaticValue('string', 3, 0x00),
+            language: reader.consumeStaticValue('string', 3, TextEncoding.ISO_8859_1),
             shortText: reader.consumeNullTerminatedValue('string'),
             text: reader.consumeStaticValue('string', null)
         }
